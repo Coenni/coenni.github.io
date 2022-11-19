abb24d74f04f5bad559c1f54f5d67b8ecb81f9cd
Nijat Suleymanov <nicat.suleymanov@gmail.com>
2022-11-19 03:44:03 +0100

    config prod

---
+++ 9442-es2015.36ca4c445976fa6fc859.js
@@ -0,0 +1,1 @@
+(self.webpackChunkphoto_gallery_ng_capacitor=self.webpackChunkphoto_gallery_ng_capacitor||[]).push([[9442],{9442:function(e,a,c){"use strict";c.r(a),c.d(a,{SplashScreenWeb:function(){return s}});var n=c(5310);class s extends n.Uw{async show(e){}async hide(e){}}}}]);
